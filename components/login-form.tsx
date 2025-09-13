3711d1114c5f0ac70dc515a19b104fad0068c8f9
Yipmong Said <Syipmong5@gmail.com>
2025-09-13 21:20:57 +0100

    fix: remove extra whitespace in LoginForm component

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -67,6 +67,7 @@ export function LoginForm({
         By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
         and <a href="#">Privacy Policy</a>.
       </div>
+      
     </div>
   )
 }
